cc9c0587a1a70486bceb6165636c869286368c5a
Skawm <skawm@users.noreply.github.com>
2018-08-04 09:45:07 +0200

    Update api.py

--- src/api.py
+++ src/api.py
@@ -3,7 +3,7 @@ import redis
 app = Flask(__name__)
 
 # Connect to RedisDB
-redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_db = redis.StrictRedis(host='redis', port=6379, db=0)
 
 
 def get_followers_count(token):
